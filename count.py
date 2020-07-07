99e0dc5da8ca26e44f4def927472e067065ca021
Mooner510 <48984409+Mooner510@users.noreply.github.com>
2020-07-08 01:18:54 +0900

    Update count.py

--- count.py
+++ count.py
@@ -15,10 +15,13 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    user = message.author.display_name
     user_id = message.author.id
     channel_id = message.channel.id
     msg = message.content
     ai = message.author.bot
+    arg = msg.split()
+    ac = len(arg) - 1
     use_channel = ['730076628490911794']
     admin_list = ['524868734054039554', '672604694463512581']
     if ai == True:
@@ -42,20 +45,45 @@ async def on_message(message):
                         await message.delete()
                         await message.channel.send("숫자를 리셋하였습니다!\n> 1부터 시작합니다")
                 else:
+                    await message.delete()
                     mm = await message.channel.send("<@" + str(user_id) + "> 너능 관리자가 아니쟈냥!")
                     time.sleep(4)
                     await mm.delete()
-            try:
-                if int(msg) == int(filed) + 1:
-                    await message.delete()
-                    await message.channel.send("<@" + str(user_id) + ">  :: **" + str(int(filed) + 1) + "**")
-
-                    with open("number.txt", 'w+', encoding='utf-8') as file:
-                        file.write(str(int(filed) + 1))
+            elif str(arg[0]) == "set":
+                if ac >= 1:
+                    if str(user_id) in admin_list:
+                        try:
+                            if int(msg) >= 0 or int(msg) <= 0:
+                            with open("number.txt", 'w+', encoding='utf-8') as file:
+                                file.write(str(int(msg)))
+                            await message.delete()
+                            await message.channel.send("숫자를 리셋하였습니다!\n> **" + str(int(msg)) + "**부터 시작합니다")
+                        except:
+                            await message.delete()
+                    else:
+                        await message.delete()
+                        mm = await message.channel.send("<@" + str(user_id) + "> 너능 관리자가 아니쟈냥!")
+                        time.sleep(4)
+                        await mm.delete()
                 else:
                     await message.delete()
-            except:
-                await message.delete()
+                    mm = await message.channel.send("<@" + str(user_id) + "> 너능 관리자가 아니쟈냥!")
+                    time.sleep(4)
+                    await mm.delete()
+            else:
+                try:
+                    if int(msg) == int(filed) + 1:
+                        await message.delete()
+                        await message.channel.send("<@" + str(user_id) + ">  :: **" + str(int(filed) + 1) + "**")
+
+                        with open("number.txt", 'w+', encoding='utf-8') as file:
+                            file.write(str(int(filed) + 1))
+                    else:
+                        await message.delete()
+                except:
+                    await message.delete()
+                    channel = bot.get_channel(self, id=727765114912112640)
+                    await channel.send(str(user) + " ▶ " + str(msg))
         else:
             with open("number.txt", 'w+', encoding='utf-8') as file:
                 file.write("0")
